a604807df3537c5163623b4b12e3711b4d80adb5
Zachary zutaut <zacharyzutaut@oldrochester.org>
2021-06-14 10:25:04 -0400

    fix bug

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-let text = document.querySelector("h1")
-
-text.addEventListener("click", function () {
-  text.style.backgroundColor = text.style.backgroundColor === "red" ? "blue" : "red";
-})
-
 let board = [
   ['', '', ''],
   ['', '', ''],
-  ['', '', ''],
+  ['', '', '']
 ];
 
 let players = ['X', 'O'];
 
 let currentPlayer;
+let available = [];
+
 function setup() {
   createCanvas(400, 400);
-  frameRate(1);
-  curentPlayer = floor(random(players.length));
+  frameRate(30);
+  currentPlayer = floor(random(players.length));
   for (let j = 0; j < 3; j++) {
     for (let i = 0; i < 3; i++) {
       available.push([i, j]);
@@ -24,8 +20,12 @@ function setup() {
   }
 }
 
+
+
+
+
 function equals3(a, b, c) {
-  return (a == b && b == c && a != '');
+  return a == b && b == c && a != '';
 }
 
 function checkWinner() {
@@ -33,29 +33,29 @@ function checkWinner() {
 
   // horizontal
   for (let i = 0; i < 3; i++) {
-    if (equals3(board[0][i], board[0][i], board[0][i]))
-      winner = board[0][i];
+    if (equals3(board[i][0], board[i][1], board[i][2]))
+      winner = board[i][0];
   }
 
   // Vertical
   for (let i = 0; i < 3; i++) {
-    if (equals3(board[0][i], board[0][i], board[0][i]))
+    if (equals3(board[0][i], board[1][i], board[2][i]))
       winner = board[0][i];
   }
-}
 
-// Diagonal
-if (equals3(board[0][i], board[0][i], board[0][i]))
-  winner = board[0][0];
+  // Diagonal
+  if (equals3(board[0][0], board[1][1], board[2][2]))
+    winner = board[0][0];
+  
+  if (equals3(board[2][0], board[1][1], board[0][2])) {
+    winner = board[2][0];
+  }
 
-if (board[2][0] == board[1][1] == board[0][2]) {
-  winner = board[2][0];
-}
-
-if (winner == null && available.length == 0) {
-  return 'tie';
-} else {
-  return winner;
+  if (winner == null && available.length == 0) {
+    return 'tie';
+  } else {
+    return winner;
+  }
 }
 
 function nextTurn() {
@@ -64,17 +64,17 @@ function nextTurn() {
   let i = spot[0];
   let j = spot[1];
   board[i][j] = players[currentPlayer];
-  currentPlayer = (curentPlayer + 1) % players.length;
+  currentPlayer = (currentPlayer + 1) % players.length;
 }
-
-// function mousePressed() {
-//   nextTurn();
-// }
-
+ // function mousePressed() {
+  //   nextTurn();
+  // }
+  
 function draw() {
   background(255);
   let w = width / 3;
   let h = height / 3;
+  strokeWeight(4);
 
   line(w, 0, w, height);
   line(w * 2, 0, w * 2, height);
@@ -83,29 +83,34 @@ function draw() {
 
   for (let j = 0; j < 3; j++) {
     for (let i = 0; i < 3; i++) {
-      let x = w * i;
-      let y = h * j;
+      let x = w * i + w / 2;
+      let y = h * j + h / 2;
       let spot = board[i][j];
       textSize(32);
-      strokeWeight(4);
-      if (spot == players[1]) {
+let r = w / 4;     
+ if (spot == players[1]) {
         noFill();
-        ellipseMode(CORNER);
+        ellipse(x, y, r * 2);
         ellipse(x, y, w / 2);
       } else if (spot == players[0]) {
         let xr = w / 4;
-        line(x - xr, y - xr, x + xr, y + xr);
-        line(x + xr.y - xr, x - xr, y + xr);
+        line(x - r, y - r, x + r, y + r);
+        line(x + r, y - r, x - r, y + r);
       }
-
     }
   }
 
-  let result = checWinner();
+  let result = checkWinner();
   if (result != null) {
-    noloop();
-    createP(result).style('color', '#FFF').style('font-size', '32pt')
-    console.log(result);
+    noLoop();
+    let resultP = createP('');
+    resultP.style('font-size', '32pt');
+    if (result == 'tie') {
+      resultP.html('Tie!');
+    } else {
+      resultP.html(`${result} wins!`);
+    }
+  } else {
+    nextTurn();
   }
-  nextTurn();
 }
